a600ae054c22c553d6d6b208f83bfd47f4b3038c
Vinixz <felixhan175@gmail.com>
2016-10-07 14:52:37 +0700

    BoldedDate

---
+++ Latihan_2_1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Latihan_2_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Calendar1 = new System.Windows.Forms.MonthCalendar();
+            this.DomUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.btn_tambah = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.NumUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.NumUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Calendar1
+            // 
+            this.Calendar1.Location = new System.Drawing.Point(95, 75);
+            this.Calendar1.Name = "Calendar1";
+            this.Calendar1.TabIndex = 0;
+            // 
+            // DomUpDown1
+            // 
+            this.DomUpDown1.Location = new System.Drawing.Point(216, 22);
+            this.DomUpDown1.Name = "DomUpDown1";
+            this.DomUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.DomUpDown1.TabIndex = 2;
+            this.DomUpDown1.Text = "Bulan";
+            // 
+            // btn_tambah
+            // 
+            this.btn_tambah.Location = new System.Drawing.Point(382, 22);
+            this.btn_tambah.Name = "btn_tambah";
+            this.btn_tambah.Size = new System.Drawing.Size(75, 23);
+            this.btn_tambah.TabIndex = 3;
+            this.btn_tambah.Text = "Tambah";
+            this.btn_tambah.UseVisualStyleBackColor = true;
+            this.btn_tambah.Click += new System.EventHandler(this.btn_tambah_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(478, 22);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(75, 23);
+            this.btn_delete.TabIndex = 4;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // NumUpDown1
+            // 
+            this.NumUpDown1.Location = new System.Drawing.Point(74, 22);
+            this.NumUpDown1.Name = "NumUpDown1";
+            this.NumUpDown1.Size = new System.Drawing.Size(69, 20);
+            this.NumUpDown1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 420);
+            this.Controls.Add(this.NumUpDown1);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_tambah);
+            this.Controls.Add(this.DomUpDown1);
+            this.Controls.Add(this.Calendar1);
+            this.Name = "Form1";
+            this.Text = "Latihan_2_1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.NumUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar Calendar1;
+        private System.Windows.Forms.DomainUpDown DomUpDown1;
+        private System.Windows.Forms.Button btn_tambah;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.NumericUpDown NumUpDown1;
+    }
+}
+
---
+++ Latihan_2_1/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latihan_2_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Calendar1.SetCalendarDimensions(2,2);
+            Calendar1.ScrollChange = 4;
+            NumUpDown1.Maximum = 31;
+            NumUpDown1.Minimum = 1;
+
+            DomUpDown1.Items.Add("Januari");
+            DomUpDown1.Items.Add("Februari");
+            DomUpDown1.Items.Add("Maret");
+            DomUpDown1.Items.Add("April");
+            DomUpDown1.Items.Add("May");
+            DomUpDown1.Items.Add("Juni");
+            DomUpDown1.Items.Add("Juli");
+            DomUpDown1.Items.Add("Agustus");
+            DomUpDown1.Items.Add("September");
+            DomUpDown1.Items.Add("Oktober");
+            DomUpDown1.Items.Add("November");
+            DomUpDown1.Items.Add("Desember");
+
+           
+            DateTime libur = new DateTime(2016, 1, 1);
+            DateTime tglLahir = new DateTime(2016, 5, 18);
+            while (libur.Year == 2016)
+            {
+                if (libur.DayOfWeek == DayOfWeek.Saturday)
+                {
+                    Calendar1.AddBoldedDate(libur);
+                    libur = libur.AddDays(1);
+                }
+                else if (libur.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    Calendar1.AddBoldedDate(libur);
+                    libur = libur.AddDays(6);
+                }
+                else
+                {
+                    libur = libur.AddDays(1);
+                }
+            }
+            Calendar1.AddAnnuallyBoldedDate(tglLahir);
+            Calendar1.UpdateBoldedDates();
+        }
+
+        private void btn_tambah_Click(object sender, EventArgs e)
+        {
+            int tgl, bln;
+            tgl = Convert.ToInt32(NumUpDown1.Value);
+            bln = Convert.ToInt32(DomUpDown1.SelectedIndex) + 1;
+
+            if(((bln == 4 || bln == 6 || bln == 9 || bln == 11) && tgl > 30 ) || (bln == 2 && tgl > 29))
+            {
+                MessageBox.Show("Invalid Date", "ERROR");
+            }
+            else{
+                DateTime tanggal = new DateTime(2016, bln, tgl);
+                Calendar1.AddAnnuallyBoldedDate(tanggal);
+                Calendar1.SetDate(tanggal);
+                Calendar1.UpdateBoldedDates();
+            }
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            DateTime selecttgl = Calendar1.SelectionRange.Start.Date;
+            Calendar1.RemoveAnnuallyBoldedDate(selecttgl);
+            Calendar1.UpdateBoldedDates();
+        }
+    }
+}
