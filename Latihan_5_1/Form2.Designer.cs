828f61a555c4f8ddb2491bb172ba168be3c063c1
Vinixz <felixhan175@gmail.com>
2016-10-26 15:08:45 +0700

    Tambah Menu Editor untuk edit Background dan Treeview

---
+++ Latihan_5_1/Form2.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Latihan_5_1
+{
+    partial class Editor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.cbxBackgroundColor = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(53, 53);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(148, 146);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // cbxBackgroundColor
+            // 
+            this.cbxBackgroundColor.FormattingEnabled = true;
+            this.cbxBackgroundColor.Location = new System.Drawing.Point(218, 53);
+            this.cbxBackgroundColor.Name = "cbxBackgroundColor";
+            this.cbxBackgroundColor.Size = new System.Drawing.Size(121, 21);
+            this.cbxBackgroundColor.TabIndex = 3;
+            this.cbxBackgroundColor.Text = "White";
+            
+            // 
+            // Editor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 263);
+            this.Controls.Add(this.cbxBackgroundColor);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Editor";
+            this.Text = "Editor";
+            this.Load += new System.EventHandler(this.Editor_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        public System.Windows.Forms.ComboBox cbxBackgroundColor;
+    }
+}
---
+++ Latihan_5_1/Form2.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Text;
+using System.Reflection;
+
+namespace Latihan_5_1
+{
+    public partial class Editor : Form
+    {
+        public Editor()
+        {
+            InitializeComponent();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Text == "BackgroundColor")
+            {
+                cbxBackgroundColor.Show();
+            }
+            else
+            {
+                cbxBackgroundColor.Hide();
+            }
+        }
+
+        private void Editor_Load(object sender, EventArgs e)
+        {
+           
+            cbxBackgroundColor.Hide();
+
+            if (treeView1.Nodes.Count < 1)
+            {
+                treeView1.Nodes.Add("Theme");
+                treeView1.Nodes[0].Nodes.Add("BackgroundColor");
+
+
+                Color color = new Color();
+                PropertyInfo[] infocolor = color.GetType().GetProperties();
+                this.cbxBackgroundColor.DrawMode = DrawMode.OwnerDrawFixed;
+                foreach (PropertyInfo i in infocolor)
+                {
+                    if (i.PropertyType == typeof(System.Drawing.Color))
+                    {
+                        cbxBackgroundColor.Items.Add(i.Name);
+                    }
+                }
+                this.cbxBackgroundColor.DrawItem += new DrawItemEventHandler(cbxBackgroundColor_DrawItem);
+            }
+        }
+
+        private void cbxBackgroundColor_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (e.Index >= 0)
+            {
+                Graphics g = e.Graphics;
+                Brush brush = new SolidBrush(e.BackColor);
+                Brush tBrush = new SolidBrush(e.ForeColor);
+                g.FillRectangle(brush, e.Bounds);
+                string s = (string)this.cbxBackgroundColor.Items[e.Index].ToString();
+                SolidBrush b = new SolidBrush(Color.FromName(s));
+                e.Graphics.DrawRectangle(Pens.Black, 2, e.Bounds.Top + 1, 31, 11);
+                e.Graphics.FillRectangle(b, 3, e.Bounds.Top + 2, 30, 10);
+                e.Graphics.DrawString(s, this.Font, Brushes.Black, 40, e.Bounds.Top);
+                brush.Dispose();
+                tBrush.Dispose();
+            }
+            e.DrawFocusRectangle();
+        }
+
+    }
+}
