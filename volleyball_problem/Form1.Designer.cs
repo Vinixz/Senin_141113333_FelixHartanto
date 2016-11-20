9de00ef08fcf282f998b5d535e5dc99934f2fb8e
Vinixz <felixhan175@gmail.com>
2016-11-20 17:19:05 +0700

    Volleyball Match

---
+++ volleyball_problem/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace volleyball_problem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_A = new System.Windows.Forms.TextBox();
+            this.tb_B = new System.Windows.Forms.TextBox();
+            this.label_A = new System.Windows.Forms.Label();
+            this.label_B = new System.Windows.Forms.Label();
+            this.btn_Hitung = new System.Windows.Forms.Button();
+            this.label_Hasil = new System.Windows.Forms.Label();
+            this.tb_Hasil = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tb_A
+            // 
+            this.tb_A.Location = new System.Drawing.Point(83, 47);
+            this.tb_A.Name = "tb_A";
+            this.tb_A.Size = new System.Drawing.Size(198, 20);
+            this.tb_A.TabIndex = 0;
+            // 
+            // tb_B
+            // 
+            this.tb_B.Location = new System.Drawing.Point(83, 87);
+            this.tb_B.Name = "tb_B";
+            this.tb_B.Size = new System.Drawing.Size(198, 20);
+            this.tb_B.TabIndex = 1;
+            // 
+            // label_A
+            // 
+            this.label_A.AutoSize = true;
+            this.label_A.Location = new System.Drawing.Point(52, 50);
+            this.label_A.Name = "label_A";
+            this.label_A.Size = new System.Drawing.Size(14, 13);
+            this.label_A.TabIndex = 2;
+            this.label_A.Text = "A";
+            // 
+            // label_B
+            // 
+            this.label_B.AutoSize = true;
+            this.label_B.Location = new System.Drawing.Point(52, 90);
+            this.label_B.Name = "label_B";
+            this.label_B.Size = new System.Drawing.Size(14, 13);
+            this.label_B.TabIndex = 3;
+            this.label_B.Text = "B";
+            // 
+            // btn_Hitung
+            // 
+            this.btn_Hitung.Location = new System.Drawing.Point(55, 124);
+            this.btn_Hitung.Name = "btn_Hitung";
+            this.btn_Hitung.Size = new System.Drawing.Size(226, 23);
+            this.btn_Hitung.TabIndex = 4;
+            this.btn_Hitung.Text = "Hitung";
+            this.btn_Hitung.UseVisualStyleBackColor = true;
+            this.btn_Hitung.Click += new System.EventHandler(this.btn_Hitung_Click);
+            // 
+            // label_Hasil
+            // 
+            this.label_Hasil.AutoSize = true;
+            this.label_Hasil.Location = new System.Drawing.Point(52, 167);
+            this.label_Hasil.Name = "label_Hasil";
+            this.label_Hasil.Size = new System.Drawing.Size(30, 13);
+            this.label_Hasil.TabIndex = 5;
+            this.label_Hasil.Text = "Hasil";
+            // 
+            // tb_Hasil
+            // 
+            this.tb_Hasil.Location = new System.Drawing.Point(83, 164);
+            this.tb_Hasil.Name = "tb_Hasil";
+            this.tb_Hasil.Size = new System.Drawing.Size(198, 20);
+            this.tb_Hasil.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 262);
+            this.Controls.Add(this.tb_Hasil);
+            this.Controls.Add(this.label_Hasil);
+            this.Controls.Add(this.btn_Hitung);
+            this.Controls.Add(this.label_B);
+            this.Controls.Add(this.label_A);
+            this.Controls.Add(this.tb_B);
+            this.Controls.Add(this.tb_A);
+            this.Name = "Form1";
+            this.Text = "volleyball_problem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_A;
+        private System.Windows.Forms.TextBox tb_B;
+        private System.Windows.Forms.Label label_A;
+        private System.Windows.Forms.Label label_B;
+        private System.Windows.Forms.Button btn_Hitung;
+        private System.Windows.Forms.Label label_Hasil;
+        private System.Windows.Forms.TextBox tb_Hasil;
+    }
+}
+
---
+++ volleyball_problem/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace volleyball_problem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static int mod = 1000000007;
+
+        static int pascal(int a, int b)
+        {
+            int[] tp = new int[b];
+            for (int i = 0; i < b; i++)
+            {
+                tp[i] = 1;
+            }
+
+            for (int i = b - 2; i > 0; i--)
+            {
+                for (int j = i; j < b - 1; j++)
+                {
+                    tp[j] += tp[j + 1];
+                    tp[j] %= mod;
+                }
+            }
+            int game = 1;
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b - 1; j++)
+                {
+                    tp[j] += tp[j + 1];
+                    tp[j] %= mod;
+                }
+
+                game += tp[0];
+                game %= mod;
+            }
+            return game;
+        }
+
+        static long expo(long x, long exp_a)
+        {
+            long result = 1;
+            while (exp_a > 0)
+            {
+                if (exp_a % 2 == 1)
+                {
+                    result *= x;
+                    result %= mod;
+                }
+                exp_a /= 2;
+                x *= x;
+                x %= mod;
+            }
+            return result % mod;
+        }
+
+        private void btn_Hitung_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(tb_A.Text);
+            int b = Convert.ToInt32(tb_B.Text); 
+
+            int temp;
+            long result;
+  
+            if (a < b)
+            {
+                temp = a;
+                a = b;
+                b = temp;
+            }
+            if (a == 25 && b <= 23)
+            {
+                if (b < 1)
+                {
+                    tb_Hasil.Text = "1";
+                }
+                else
+                {
+                    result = (long)pascal(a - 1, b);
+                    tb_Hasil.Text = result.ToString();
+                }
+            }
+            else if (a > 25 && b == a - 2)
+            {
+                result = (long)pascal(24, 24);
+                int exp_a = b - 24;
+                result *= expo(2, exp_a);
+                result %= mod;
+                tb_Hasil.Text = result.ToString();
+            }
+            else
+            {
+                tb_Hasil.Text = "0";
+            }
+        }
+    }
+}
